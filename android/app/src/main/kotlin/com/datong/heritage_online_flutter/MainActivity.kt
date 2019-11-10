b5d0946c8239371a6a4c9a2e399de8440a90c3a6
孙楷 <704725181@qq.com>
2019-11-10 19:39:37 +1100

    Initial commit

---
+++ android/app/src/main/kotlin/com/datong/heritage_online_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.datong.heritage_online_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
