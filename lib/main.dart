dd88dbfd2b263e2c65a72e978f1e249d22e417b9
孙楷 <704725181@qq.com>
2019-11-12 11:05:51 +1100

    首页新闻列表样式
    1.同步安卓端样式

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,19 @@ import 'package:http/http.dart' as http;
 
 void main() => runApp(SimpleApp());
 
-class SimpleApp extends StatelessWidget{
+class SimpleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      title:'Sample App',
+      title: 'E-heritage',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: SampleAppPage(),
     );
   }
 }
 
-class SampleAppPage extends StatefulWidget{
-  SampleAppPage({Key key}):super(key:key);
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
@@ -26,35 +25,99 @@ class SampleAppPage extends StatefulWidget{
   }
 }
 
-class _SimpleAppPageState extends State<SampleAppPage>{
-  List widgets=[];
+class _SimpleAppPageState extends State<SampleAppPage> {
+  List widgets = [];
+
   @override
   void initState() {
     super.initState();
     loadData();
   }
 
+  getBody() {
+    if (showLoadingDialog()) {
+      return getProgressDialog();
+    } else {
+      return getListView();
+    }
+  }
+
+  getProgressDialog() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  getListView() {
+    return ListView.builder(
+      itemCount: widgets.length,
+      itemBuilder: (BuildContext context, int position) {
+        return getRow(position);
+      },
+    );
+  }
+
+  showLoadingDialog() {
+    if (widgets.length == 0) {
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sample App"),
-      ),
-      body: ListView.builder(itemCount: widgets.length,itemBuilder: (BuildContext context, int position){
-        return getRow(position);
-      },),
-    );
-  }
-  Widget getRow(int i){
-    return Padding(padding: EdgeInsets.all(10),
-    child: Text("Row ${widgets[i]["title"]}"),);
+        appBar: AppBar(
+          title: Text("E-heritage"),
+        ),
+        body: getBody());
   }
 
-  loadData()async{
-    String url="https://sunkai.xyz:5001/api/NewsList";
-    http.Response response=await http.get(url);
+  Widget getRow(int i) {
+    return Container(
+      color: Color.fromARGB(255, 230, 230, 230),
+      padding: EdgeInsets.all(4),
+      child: Card(
+        elevation: 2,
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Image(
+                image: NetworkImage(
+                    "https://sunkai.xyz:5001/img/${widgets[i]["img"]}"),
+                fit: BoxFit.contain,
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "${widgets[i]["title"]}",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "${widgets[i]["date"]}",
+                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                    Text("${widgets[i]["content"]}",
+                        style: TextStyle(fontSize: 16))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  loadData() async {
+    String url = "https://sunkai.xyz:5001/api/NewsList";
+    http.Response response = await http.get(url);
     setState(() {
-      widgets=json.decode(response.body);
+      widgets = json.decode(response.body);
     });
   }
 }
